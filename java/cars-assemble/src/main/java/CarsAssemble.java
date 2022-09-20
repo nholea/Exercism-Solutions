9d9e33046a281a1278947911c710a9facc836e07
Natalia H <nholea@gmail.com>
2022-09-20 23:22:44 +0200

    feat: added productionRatePerHour and workingItemsPerMinute methods

---
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -0,0 +1,23 @@
+public class CarsAssemble {
+
+    public double productionRatePerHour(int speed) {
+        int CAR_PRODUCED_PER_HOUR = 221;
+        return CAR_PRODUCED_PER_HOUR * speed * successRate(speed);
+    }
+
+
+    public int workingItemsPerMinute(int speed) {
+        return (int) (productionRatePerHour(speed) / 60);
+    }
+    private static double successRate(int speed) {
+        if (speed >= 1 && speed < 5) {
+            return 1.;
+        } else if (speed >= 5 && speed < 9) {
+            return 0.9;
+        } else if (speed < 10) {
+            return 0.8;
+        } else {
+            return 0.77;
+        }
+    }
+}
