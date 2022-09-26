4b3f8f490c71d48933dd39facb186ae18aca5101
Natalia H <nholea@gmail.com>
2022-09-26 21:03:08 +0200

    feat: implement getHammingDistance method

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,34 @@
+import java.util.Arrays;
+
 public class Hamming {
+    private final String leftStrand;
+    private final String rightStrand;
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() > rightStrand.length() && rightStrand.length()!=0) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        if (rightStrand.length() > leftStrand.length() && leftStrand.length() !=0){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        if (rightStrand.length() == 0 && leftStrand.length() !=0){
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+        if (leftStrand.length() == 0 && rightStrand.length() !=0){
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+        this.rightStrand = rightStrand;
+        this.leftStrand = leftStrand;
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int counter = 0;
+
+        for (int index = 0; index < rightStrand.length(); index++) {
+            if (rightStrand.charAt(index) != leftStrand.charAt(index)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
