810901eba1d5cae5e6df95f22940c9137f98b8c8
Natalia H <nholea@gmail.com>
2022-09-27 22:54:50 +0200

    feat: implement convert method

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,24 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String result = "";
+
+        if(number % 3 == 0) {
+            result +="Pling";
+        }
+        if(number % 5 == 0) {
+            result +="Plang";
+
+        }
+        if (number % 7 == 0) {
+            result +="Plong";
+
+        }
+        if (result.isEmpty()){
+            result = String.valueOf(number);
+        }
+
+        return result;
     }
 
 }
